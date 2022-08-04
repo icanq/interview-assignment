ee94ce07d7491739fa8cdd39dbda05be9f6c8175
alaqsaka <aqsaabidin15@gmail.com>
2022-08-04 22:09:20 +0700

    writing more tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 // Tes 0: render header
@@ -8,11 +8,50 @@ test("should render header", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-// tes 1: should render input for creating new task
-it("should render input for creating new task", () => {
-  render(<App />);
-  const inputElement = screen.getByPlaceholderText(
-    /Masukan nama tugas yang perlu dilakukan .../i
-  );
-  expect(inputElement).toBeInTheDocument();
+describe("Creating a new task", () => {
+  // tes 1: should render input for creating new task
+  it("should render input", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText(
+      /Masukan nama tugas yang perlu dilakukan .../i
+    );
+
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  // tes 2: should render input for handling submit
+  it("should render button tambah", () => {
+    render(<App />);
+    const buttonTambah = screen.getByRole("button", { name: /Tambah/i });
+
+    expect(buttonTambah).toBeInTheDocument();
+  });
+
+  // tes 3: should be able to type in input
+  it("should be able to type in input", () => {
+    render(<App />);
+    // Mendapatkan input berdasarkan placeholder
+    const inputElement = screen.getByPlaceholderText(
+      /Masukan nama tugas yang perlu dilakukan .../i
+    );
+
+    // Setelah berhasil dapet input, ketik sesuatu ke input tersebut
+    fireEvent.change(inputElement, { target: { value: "Bersihin Kamar" } });
+    expect(inputElement.value).toBe("Bersihin Kamar");
+  });
+
+  // tes 4: value of the input should be an empty string after cliked button tambah
+  it("value of the input should be an empty string after cliked button tambah", () => {
+    render(<App />);
+    // Mendapatkan input berdasarkan placeholder
+    const inputElement = screen.getByPlaceholderText(
+      /Masukan nama tugas yang perlu dilakukan .../i
+    );
+
+    // Setelah berhasil dapet input, ketik sesuatu ke input tersebut
+    fireEvent.change(inputElement, { target: { value: "Bersihin Kamar" } });
+    const buttonTambah = screen.getByRole("button", { name: /Tambah/i });
+    fireEvent.click(buttonTambah);
+    expect(inputElement.value).toBe("");
+  });
 });
