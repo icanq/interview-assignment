92b14b49564c4b0c0dee260c7dc3227f9cfbeed4
aegisfiveniner <aegis.five.niner@gmail.com>
2022-08-09 15:15:49 +0700

    fix: test expectaion

--- client/src/TodoList.test.js
+++ client/src/TodoList.test.js
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import TodoList from "./TodoList";
 
 test("Renders Todo List Correctly", async () => {
-  render(<TodoList tasks={["masak ikan", "minum air"]} />);
+  render(<TodoList tasks={[{ name: "masak ikan" }, { name: "minum air" }]} />);
   expect(screen.getByText(/masak ikan/i)).toBeInTheDocument();
   expect(screen.getByText(/minum air/i)).toBeInTheDocument();
 });
